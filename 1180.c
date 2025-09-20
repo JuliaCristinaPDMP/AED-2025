c6603c9af068fcf26000877fa7b3e9353aaf0a87
Júlia Cristina <163027963+JuliaCristinaPDMP@users.noreply.github.com>
2025-09-19 22:51:42 -0300

    Create 1180.c

---
+++ 1180.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int valor, menor_valor, posicao;
+
+    scanf("%d", &menor_valor); // primeiro valor
+    posicao = 0;
+
+    for (int i = 1; i < N; i++) {
+        scanf("%d", &valor);
+        if (valor < menor_valor) {
+            menor_valor = valor;
+            posicao = i;
+        }
+    }
+
+    printf("Menor valor: %d\n", menor_valor);
+    printf("Posicao: %d\n", posicao);
+
+    return 0;
+}
